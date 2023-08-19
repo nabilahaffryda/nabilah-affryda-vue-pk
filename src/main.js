7ee02af74974d8fb9fa4d086a157e420078b0748
nabilahaffryda <sehungantengs@gmail.com>
2023-08-19 17:18:33 +0700

    update photography web

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "./scss/main.scss";
+import VueRouter from "vue-router";
+import Vuelidate from "vuelidate";
+import axios from "axios";
+import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import NotFound from "./views/NotFound";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.use(Vuelidate);
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+    { path: "/contact", component: Contact },
+    { path: "*", component: NotFound },
+  ],
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
